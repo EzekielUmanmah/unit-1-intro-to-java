bb794a4b04520f688261a784e604c693ec8edf5d
Ezekiel <umanmah234@gmail.com>
2023-01-12 13:25:15 -0500

    add greeting

---
+++ src/GuessingGame.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+
+public class GuessingGame {
+    public GuessingGame(){
+        String name;
+    }
+
+    public static void main(String[] args){
+        System.out.println("Hello there! Welcome to the Guessing Game!");
+    }
+}
