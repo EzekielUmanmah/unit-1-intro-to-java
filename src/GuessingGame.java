f37ae3bd43d859d24fb2c1df74c08a28e1d3b2fb
Ezekiel <umanmah234@gmail.com>
2023-01-12 13:28:10 -0500

    add get player name

--- src/GuessingGame.java
+++ src/GuessingGame.java
@@ -8,5 +8,9 @@ public class GuessingGame {
 
     public static void main(String[] args){
         System.out.println("Hello there! Welcome to the Guessing Game!");
+
+        System.out.println("Please enter your name: ");
+        Scanner scan = new Scanner(System.in);
+        String playerName = scan.nextLine();
     }
 }
